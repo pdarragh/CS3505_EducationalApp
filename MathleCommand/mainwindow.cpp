25c1794b2ef70611df12a0539095825bc0a7a662
Melina Casanova <aury.casanova@gmail.com>
2015-12-01 15:12:49 -0700

    Updating so we can add stacked widgets

--- MathleCommand/mainwindow.cpp
+++ MathleCommand/mainwindow.cpp
@@ -22,10 +22,47 @@ MainWindow::MainWindow(QWidget *parent) :
    EquationGenerator* equations = new EquationGenerator;
    equations->generateAddition();
    equations->generateSubtraction();
-}
 
+   // TODO: Check if user is student or teacher
+   teacher = false;
+
+   // Creates the appropriate account display type for the user
+   if (teacher)
+   {
+       displayTeacherAccount();
+   }
+   else
+   {
+       displayStudentAccount();
+   }
+}
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/*
+ * displayTeacherAccount()
+ *
+ * Causes the display for a teacher to appear
+ */
+displayTeacherAccount()
+{
+
+}
+
+/*
+ * displayStudentAccount()
+ *
+ * Causes the display for a student to appear. This should include buttons linking to the game's
+ * levels and a display of that level's current high score; a large display area for the student's
+ * name; a main menu button that opens the game in a new tab; and a button linking to the scoreboard.
+ */
+displayStudentAccount()
+{
+   QPushButton main_menu("Main Menu", this);
+   main_menu->setGeometry(QRect(QPoint(100, 150), QSize(100, 100)));
+
+
 }
