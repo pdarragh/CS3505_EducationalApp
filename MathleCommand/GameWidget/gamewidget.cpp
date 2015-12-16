a973ce0fb91ff896e3c511c929013fa2580c5bff
deadlyburger <u0823377@utah.edu>
2015-12-16 11:56:36 -0700

    Fixed problems, mathles fall and get deleted. implemented lives

--- MathleCommand/GameWidget/gamewidget.cpp
+++ MathleCommand/GameWidget/gamewidget.cpp
@@ -21,7 +21,7 @@ GameWidget::GameWidget(QWidget *parent) :
     sceneQueue(QQueue<QGraphicsRectItem*>())
 {
     ui->setupUi(this);
-    gravity = 100; //Higher number = slower fall, cannot equal 0
+    gravity = 50; //Higher number = slower fall, cannot equal 0
 
     equations = new EquationGenerator;
     ui->equation->setText(equations->generateEquations(EquationGenerator::Addition));
@@ -77,16 +77,28 @@ void GameWidget::on_answerButton_clicked()
 
 void GameWidget::update()
 {
-    if (count >= 10) {
+    if (count >= 30) {
         count = 0;
         //Create a bomb
         // 0,0,0    x,0,x               0,0,x
         // Regular  Regular with shift  Upward arc
-        int x = rand() % ui->graphicsView->contentsRect().width();
+        int x = rand() % (ui->graphicsView->width() / 4);
+        qDebug() << x;
         mathleQueue.enqueue(new Mathle(x, 0, x+2));
         sceneQueue.enqueue(scene->addRect(mathleQueue.last()->posx, mathleQueue.last()->posy, 10, 10,QPen(Qt::red), QBrush(Qt::SolidPattern)));
     }
     else count++;
+
+    for (int i = 0; i < sceneQueue.size(); i++) {
+        mathleQueue[i]->posy = pow(mathleQueue[i]->posx - mathleQueue[i]->posxStart, 2)/gravity;
+        if (mathleQueue[i]->posxStart <= (ui->graphicsView->contentsRect().width() / 8)) {
+            sceneQueue[i]->setPos(mathleQueue[i]->posx++, mathleQueue[i]->posy);
+        }
+        else {
+            sceneQueue[i]->setPos(mathleQueue[i]->posx--, mathleQueue[i]->posy);\
+        }
+    }
+
     int highestMathley = 0;
     //TODO: use iterator instead of for-loop
     for(int i=0; i<sceneQueue.length(); i++){
@@ -96,27 +108,22 @@ void GameWidget::update()
     // If a mathle has NOT hit the ground.
     if(highestMathley < ui->graphicsView->contentsRect().height()-10)
     {
-        qDebug() << "fuf me";
-        //double oldy = mathle1->posy;
-        // Don't ask why, but for some reason posx and posy have to be assigned here.
-        // y = (x-offSet)^2 / slowDown;
-        // THEY ARE PUT IN AT THE TOP FIRST
-        // If offset and x > 250, send it left
-        for (size_t i = 0; i < sceneQueue.size(); i++) {
-            mathleQueue[i]->posy = pow(mathleQueue[i]->posx - mathleQueue[i]->posxStart, 2)/gravity;
-            sceneQueue[i]->setPos(mathleQueue[i]->posx, mathleQueue[i]->posy);
-        }
-
-//        qDebug() << mathle1->posx << mathle1->posy << mathle1->posy-oldy;
-//        qDebug() << mathle2->posx << mathle2->posy << mathle2->posxStart;
-//        qDebug() << mathle3->posx << mathle3->posy;
-//        qDebug() << mathle4->posx << mathle4->posy;
 
     }
     // Mathle hit the ground
     else
-    {
-        qDebug() << "OVER";
+    {  
+        lives--;
+
+        if (lives <= 0) {
+            qDebug() << "gameOVER";
+            delete(this);
+
+        }
+        else {
+            delete(sceneQueue.dequeue());
+            delete(mathleQueue.dequeue());
+        }
 /*
         //scene->addRect(0,0, 10, 10,QPen(), QBrush(Qt::SolidPattern));
         QGraphicsPixmapItem *q=new QGraphicsPixmapItem(QPixmap(":/new/prefix1/amissile.png"));
